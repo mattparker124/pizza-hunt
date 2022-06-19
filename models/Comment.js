53884193b969c2f58873f87308c73e82821893fa
Matthew Parker <mattparker124@gmail.com>
2022-06-19 18:40:53 -0500

    add replies and associations

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,53 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const ReplySchema = new Schema(
+    {
+        replyBody: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
+);
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
